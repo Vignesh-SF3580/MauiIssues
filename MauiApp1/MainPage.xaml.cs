8188f946db80ae70acbd66bdd357159eaf8f7fe9
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-03-21 16:11:25 +0530

    Add files via upload

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        Console.WriteLine("Created");
+        InitializeComponent();
+    }
+
+    ~MainPage()
+    {
+        Console.WriteLine("Destroyed");
+    }
+
+    private void OnPushClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MainPage(), true);
+    }
+
+    private void OnPopClicked(object sender, EventArgs e)
+    {
+        if (Navigation.NavigationStack.Count > 0)
+            Navigation.PopAsync(true);
+    }
+
+    private void OnPopToRootClicked(object sender, EventArgs e)
+    {
+        Navigation.PopToRootAsync(true);
+    }
+}
